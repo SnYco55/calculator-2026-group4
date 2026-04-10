6477ece4a478da74b7ad4c456e1a6371bea4855b
lmoul <leomoulin125@gmail.com>
2026-04-10 15:17:50 +0200

    Add a lot of javadoc

--- src/main/java/calculator/Pow.java
+++ src/main/java/calculator/Pow.java
@@ -2,15 +2,42 @@ package calculator;
 
 import java.util.List;
 
+/**
+ * Pow represents exponentiation (power) operation.
+ * Extends Operation to compute base raised to the power of exponent.
+ *
+ * @see Operation
+ */
 public class Pow extends Operation{
+    /**
+     * Constructs a Pow operation with default notation.
+     *
+     * @param elist the list of expressions
+     * @throws IllegalConstruction if the expression list is invalid
+     */
     public /*constructor*/ Pow(List<Expression> elist) throws IllegalConstruction {
         this(elist, null);
     }
 
+    /**
+     * Constructs a Pow operation with specified notation.
+     *
+     * @param elist the list of expressions
+     * @param n the notation for this operation
+     * @throws IllegalConstruction if the expression list is invalid
+     */
     public Pow(List<Expression> elist, Notation n) throws IllegalConstruction {
         super(elist,n);
     }
 
+    /**
+     * Computes the exponentiation of two values.
+     * Converts operands to complex numbers and performs power operation.
+     *
+     * @param l the base value
+     * @param r the exponent value
+     * @return the result of l raised to the power r
+     */
     public Value op(Value l, Value r) {
         Value result = dispatch(l, r);
 
@@ -22,16 +49,37 @@ public class Pow extends Operation{
         return Operation.format(new MyReal(left.getReal().pow(right.getReal().intValue())).toComplex());
     }
 
+    /**
+     * Computes the exponentiation of two rational numbers.
+     *
+     * @param left the base rational
+     * @param right the exponent rational
+     * @return the result of exponentiation
+     */
     @Override
     public Value opRat(MyRational left, MyRational right) {
         return op(new MyReal(left.getValue()), new MyReal(right.getValue()));
     }
 
+    /**
+     * Computes the exponentiation of a rational and a number.
+     *
+     * @param left the base rational
+     * @param right the exponent number
+     * @return the result of exponentiation
+     */
     @Override
     public Value opRatNum(MyRational left, MyNumber right) {
         return op(new MyReal(left.getValue()), right);
     }
 
+    /**
+     * Computes the exponentiation of a number and a rational.
+     *
+     * @param left the base number
+     * @param right the exponent rational
+     * @return the result of exponentiation
+     */
     @Override
     public Value opNumRat(MyNumber left, MyRational right) {
         return op(left, new MyReal(right.getValue()));
--- src/main/java/calculator/Scientific.java
+++ src/main/java/calculator/Scientific.java
@@ -3,11 +3,30 @@ package calculator;
 import java.math.BigDecimal;
 import java.util.List;
 
+/**
+ * Scientific represents scientific notation computation (base E power).
+ * Extends Operation to support arithmetic expressions with scientific notation formatting.
+ *
+ * @see Operation
+ */
 public class Scientific extends Operation {
+    /**
+     * Constructs a Scientific operation with default notation.
+     *
+     * @param elist the list of expressions
+     * @throws IllegalConstruction if the expression list is invalid
+     */
     public Scientific(List<Expression> elist) {
         this(elist, null);
     }
 
+    /**
+     * Constructs a Scientific operation with specified notation.
+     *
+     * @param elist the list of expressions
+     * @param n the notation for this operation
+     * @throws IllegalConstruction if the expression list is invalid
+     */
     public Scientific(List<Expression> elist, Notation n) throws IllegalConstruction {
         super(elist,n);
     }
@@ -20,16 +39,40 @@ public class Scientific extends Operation {
         return new MyReal(new BigDecimal(base + "E" +  power));
     }
 
+    /**
+     * Performs scientific notation on two rational numbers.
+     * Not supported in this implementation.
+     *
+     * @param left the left operand
+     * @param right the right operand
+     * @return null
+     */
     @Override
     public Value opRat(MyRational left, MyRational right) {
         return null;
     }
 
+    /**
+     * Performs scientific notation on a rational and a number.
+     * Not supported in this implementation.
+     *
+     * @param left the left rational operand
+     * @param right the right number operand
+     * @return null
+     */
     @Override
     public Value opRatNum(MyRational left, MyNumber right) {
         return null;
     }
 
+    /**
+     * Performs scientific notation on a number and a rational.
+     * Not supported in this implementation.
+     *
+     * @param left the left number operand
+     * @param right the right rational operand
+     * @return null
+     */
     @Override
     public Value opNumRat(MyNumber left, MyRational right) {
         return null;
